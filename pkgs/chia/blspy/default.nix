2ba568aa2296ee297514872d166744ea10f8109f
Break Yang <breakds@gmail.com>
2021-04-16 20:28:59 -0700

    Update the license of blspy

--- pkgs/chia/blspy/default.nix
+++ pkgs/chia/blspy/default.nix
@@ -32,7 +32,7 @@ in buildPythonPackage rec {
   meta = with lib; {
     description = "BLS Signatures implementation";
     homepage = "https://github.com/Chia-Network/bls-signatures";
-    license = licenses.mit;
+    license = licenses.asl20;
     maintainers = with maintainers; [ breakds ];
   };
 }
