9b9368629903409f777dd5f32c8b281146f80ba4
Break Yang <breakds@gmail.com>
2021-05-14 13:01:46 -0700

    Update chiafan-monitor

--- pkgs/chiafan-monitor/default.nix
+++ pkgs/chiafan-monitor/default.nix
@@ -1,4 +1,5 @@
-{ buildPythonApplication, setuptools, click, flask, fetchFromGitHub }:
+{ buildPythonApplication, setuptools, click, flask, requests,
+  fetchFromGitHub }:
 
 buildPythonApplication rec {
   pname = "chiafan-monitor";
@@ -7,11 +8,11 @@ buildPythonApplication rec {
   src = fetchFromGitHub {
     owner = "chiafan-org";
     repo = "chiafan-monitor";
-    rev = "853c46d9c7ec7991b249791121066e5724a67934";
-    sha256 = "sha256-xAUmUn2ZHRSyDQGNm5jz1YmdjmudjvftWDf5ZdwVAQK=";
+    rev = "d59715a3cb9f6a611d00a3b11b89aedde4110fa2";
+    sha256 = "sha256-K/kaAjEMZDyLEGqC892YmgoCMgFdgoCSXfXR0fovwjk=";
   };
 
   propagatedBuildInputs = [
-    setuptools click flask
+    setuptools click flask requests
   ];
 }
