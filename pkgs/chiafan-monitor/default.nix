560611656b8051382f9693bf4eacf8cad993b7fb
Break Yang <breakds@gmail.com>
2021-05-16 14:48:14 -0700

    Update chiafan-monitor

--- pkgs/chiafan-monitor/default.nix
+++ pkgs/chiafan-monitor/default.nix
@@ -8,8 +8,8 @@ buildPythonApplication rec {
   src = fetchFromGitHub {
     owner = "chiafan-org";
     repo = "chiafan-monitor";
-    rev = "d59715a3cb9f6a611d00a3b11b89aedde4110fa2";
-    sha256 = "sha256-K/kaAjEMZDyLEGqC892YmgoCMgFdgoCSXfXR0fovwjk=";
+    rev = "afde6a30f7802d4c8c3efcfc1bcfbb8528051ec0";
+    sha256 = "sha256-zUcXNc7a1CdHWLCCc/T6k5k7Np6VQcNEruOA7yFm6ok=";
   };
 
   propagatedBuildInputs = [
