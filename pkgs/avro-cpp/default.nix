82b6e642633d4a53a12ee26af3730aeb6f706173
Break Yang <breakds@gmail.com>
2021-05-17 14:13:33 -0700

    Update avro-cpp

--- pkgs/avro-cpp/default.nix
+++ pkgs/avro-cpp/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "avro-cpp";
-  version = "1.0";
+  version = "1.11.0";
 
   src = fetchFromGitHub {
     owner = "breakds";
     repo = "avro";
-    rev = "ed7c7673eb444d6349fe4d2eb496d7ba83d42b9e";
-    sha256 = "sha256-xjHY9ODOXR1V5lOtHWUd5GLog4IN298PwtOagzcbfTY=";
+    rev = "a1da5e854c45fbf7a485c4bde106e628aff1d30f";
+    sha256 = "sha256-hP/bG7MxgffwyzJUWcbAo+oGB6LA2hqqAP6/XDD7W50=";
   };
 
   cmakeDir = "../lang/c++";
