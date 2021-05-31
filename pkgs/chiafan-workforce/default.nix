e6234eca28d1706f739465da6297cd8cd5c2eb9f
Break Yang <breakds@gmail.com>
2021-05-31 12:14:17 -0700

    Upgrade chiafan-workforce

--- pkgs/chiafan-workforce/default.nix
+++ pkgs/chiafan-workforce/default.nix
@@ -7,8 +7,8 @@ buildPythonApplication rec {
   src = fetchFromGitHub {
     owner = "chiafan-org";
     repo = "chiafan-workforce";
-    rev = "50816f23f783059b1c8dd0ae944e90fb142e7df7";
-    sha256 = "sha256-ljptIM1NlJivbuu+fUTBnOZTFf3O4uw67t+d8mVnv+E=";
+    rev = "4e37a31f14d27bcfa6f963e8b10dcea3bd8c80ad";
+    sha256 = "sha256-C8Eg/WLkLLm9FekncvluZodCzz9WCeojOBaz2wgxVR4=";
   };
 
   propagatedBuildInputs = [
