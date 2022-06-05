0ef5ccf8215012eb1c9aad06dd136288d252f390
Break Yang <breakds@gmail.com>
2022-06-05 16:55:20 -0700

    Upgrade to 22.05

--- flake.nix
+++ flake.nix
@@ -6,11 +6,11 @@
   '';
 
   inputs = {
-    nixpkgs-unstable.url = "github:NixOS/nixpkgs?rev=f924460e91cba6473c5dc4b8ccb1a1cfc05bc2d7";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = {self, nixpkgs, nixpkgs-unstable, flake-utils, ... }:
+  outputs = {self, nixpkgs, flake-utils, ... }:
 
     let supportedLinuxSystems = [ "x86_64-linux" "i686-linux" "aarch64-linux" ];
 
@@ -18,7 +18,6 @@
       inherit supportedLinuxSystems;
       overlay = final: prev: {
         popl = final.callPackage ./pkgs/popl {};
-        nlohmann_json = final.callPackage ./pkgs/nlohmann_json {};
         clickhouse-cpp = final.callPackage ./pkgs/clickhouse-cpp {};
         highwayhash = final.callPackage ./pkgs/highwayhash {};
         avro-cpp = final.callPackage ./pkgs/avro-cpp {};  # Updated with my CMake packaging patch
@@ -26,14 +25,7 @@
         influxdb-cxx = final.callPackage ./pkgs/influxdb-cxx {};
         hiredis = final.callPackage ./pkgs/hiredis {};
         civetweb = final.callPackage ./pkgs/civetweb {};
-        chia = nixpkgs-unstable.legacyPackages.chia;
         vscode-include-fix = final.callPackage ./pkgs/vscode-include-fix {};
-        python3 = prev.python3.override {
-          packageOverrides = python-final: python-prev: {
-            chiafan-workforce = python-final.callPackage ./pkgs/chiafan-workforce {};
-            chiafan-monitor = python-final.callPackage ./pkgs/chiafan-monitor {};
-          };
-        };
       };
     } // flake-utils.lib.eachSystem supportedLinuxSystems
       (system: let pkgs = import nixpkgs {
@@ -44,8 +36,6 @@
                in {
                  packages = {
                    inherit (pkgs) popl nlohmann_json clickhouse-cpp ethminer highwayhash avro-cpp;
-                   chiafan-workforce = pkgs.python3Packages.chiafan-workforce;
-                   chiafan-monitor = pkgs.python3Packages.chiafan-monitor;
                    civetweb = pkgs.civetweb;
                    hiredis = pkgs.hiredis;
                    redis-plus-plus = pkgs.redis-plus-plus;
