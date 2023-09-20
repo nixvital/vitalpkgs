f2e6bb8f5a4be0a4f445bf78bb6a3fa616c29ed9
Sola1Go <30356570@qq.com>
2023-09-20 10:12:27 +0800

    test -v

--- pkgs/vscode-include-fix/default.nix
+++ pkgs/vscode-include-fix/default.nix
@@ -1,7 +1,11 @@
-{ python38Packages, writeShellScriptBin, llvmPackages_11 }:
+{ python38Packages, writeShellScriptBin, llvmPackages_11,llvmPackages_16 }:
 let 
   cli = python38Packages.callPackage ./python-warp.nix {};
 in 
   writeShellScriptBin "vscode-include-fix" ''
+    if [ "$1" = "-v" ] && [ "$2" = "11" ]; then
+      clang_path=${llvmPackages_11.clang}/bin/clang
+    else
+      clang_path=${llvmPackages_16.clang}/bin/clang
     ${cli}/bin/vscode-include-fix cpp-json --clang ${llvmPackages_11.clang}/bin/clang
   ''
